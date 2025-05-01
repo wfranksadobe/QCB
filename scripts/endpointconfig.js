477793c8655c9e068efefe385f36f7ff082221f2
William Franks <136557841+wfranksadobe@users.noreply.github.com>
2025-05-01 16:40:54 +1000

    updated endpoints to my new env

--- scripts/endpointconfig.js
+++ scripts/endpointconfig.js
@@ -1,9 +1,9 @@
 function getAEMPublish() {
-  return 'https://publish-p137176-e1382526.adobeaemcloud.com';
+  return 'https://publish-p154716-e1630108.adobeaemcloud.com';
 }
 
 function getAEMAuthor() {
-  return 'https://author-p137176-e1382526.adobeaemcloud.com';
+  return 'https://author-p154716-e1630108.adobeaemcloud.com';
 }
 
 export { getAEMPublish, getAEMAuthor };
